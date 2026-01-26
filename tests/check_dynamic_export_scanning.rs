1e2b7a822db715f8316d7616cd0de0a6b0d23395
Soham Nandy <soham.nandy2006@gmail.com>
2026-01-27 04:26:41 +0530

    updated the test to use the new api

--- tests/check_dynamic_export_scanning.rs
+++ tests/check_dynamic_export_scanning.rs
@@ -1,27 +1,29 @@
-use libwinexploit::runtime::exports::find_dll_base;
-use libwinexploit::runtime::exports::find_dll_export;
+use libwinexploit::runtime::pe64_runtime::PE64Runtime;
 use windows_sys::w;
-
 type LoadLibraryWFn = unsafe extern "system" fn(*const u16) -> *mut core::ffi::c_void;
 type MessageBoxWFn =
     unsafe extern "system" fn(*mut core::ffi::c_void, *const u16, *const u16, u32) -> i32;
 
 fn resolve_load_library() -> LoadLibraryWFn {
-    let kernel_32: u64 = find_dll_base("KERNEL32.DLL").unwrap();
-    let load_library: u64 = find_dll_export("LoadLibraryW", kernel_32).unwrap();
-    let load_library: LoadLibraryWFn = unsafe { core::mem::transmute(load_library) };
+    let kernel32 = PE64Runtime::from_module("KERNEL32.DLL").expect("kernel32.dll not found");
 
-    let user32_name = w!("User32.dll");
+    let load_library_addr = kernel32
+        .find_export("LoadLibraryW")
+        .expect("LoadLibraryW not found")
+        .func_addr as usize;
 
-    let user32_handle = unsafe { load_library(user32_name) };
-    assert!(!user32_handle.is_null());
-    load_library
+    unsafe { core::mem::transmute(load_library_addr) }
 }
 
 fn resolve_message_box() -> MessageBoxWFn {
-    let user32 = find_dll_base("USER32.DLL").unwrap();
-    let addr = find_dll_export("MessageBoxW", user32).unwrap();
-    unsafe { core::mem::transmute(addr) }
+    let user32 = PE64Runtime::from_module("USER32.DLL").expect("user32.dll not found");
+
+    let message_box_addr = user32
+        .find_export("MessageBoxW")
+        .expect("MessageBoxW not found")
+        .func_addr as usize;
+
+    unsafe { core::mem::transmute(message_box_addr) }
 }
 
 #[cfg(all(test, windows))]
@@ -31,35 +33,40 @@ mod tests {
 
     #[test]
     fn kernel32_is_loaded() {
-        let base = find_dll_base("KERNEL32.DLL");
-        assert!(base.is_ok());
-        assert!(base.unwrap() != 0);
+        let kernel32 = PE64Runtime::from_module("KERNEL32.DLL");
+        assert!(kernel32.is_ok());
     }
 
     #[test]
     fn can_resolve_loadlibraryw() {
-        let ll = resolve_load_library();
-        let name = w!("Kernel32.dll");
+        let load_library = resolve_load_library();
 
-        let h = unsafe { ll(name) };
+        let name = w!("Kernel32.dll");
+        let h = unsafe { load_library(name) };
+
         assert!(!h.is_null());
     }
 
     #[test]
     fn user32_can_be_loaded_manually() {
-        let ll = resolve_load_library();
-        let name = w!("User32.dll");
+        let load_library = resolve_load_library();
 
-        let h = unsafe { ll(name) };
+        let name = w!("User32.dll");
+        let h = unsafe { load_library(name) };
         assert!(!h.is_null());
 
-        let base = find_dll_base("USER32.DLL");
-        assert!(base.is_ok());
+        let user32 = PE64Runtime::from_module("USER32.DLL");
+        assert!(user32.is_ok());
     }
 
     #[test]
     fn can_resolve_messageboxw_symbol() {
-        let kl = resolve_load_library(); // we need to call this so that user32 dll is loaded into memory for the test
+        // Ensure USER32 is loaded
+        let load_library = resolve_load_library();
+        unsafe {
+            load_library(w!("User32.dll"));
+        }
+
         let _ = resolve_message_box(); // should not panic
     }
 }
@@ -68,15 +75,18 @@ mod tests {
 #[test]
 #[ignore = "spawns UI"]
 fn messagebox_smoke_test() {
-    let _ = resolve_load_library(); // we need to call this so that user32 dll is loaded into memory for the test
+    let load_library = resolve_load_library();
+    unsafe {
+        load_library(w!("User32.dll"));
+    }
 
-    let msgbox = resolve_message_box();
+    let message_box = resolve_message_box();
 
     let text = w!("Manual MessageBoxW test");
     let caption = w!("PE loader");
 
     unsafe {
-        let ret = msgbox(core::ptr::null_mut(), text, caption, 0);
+        let ret = message_box(core::ptr::null_mut(), text, caption, 0);
         assert_eq!(ret, 1);
     }
 }
