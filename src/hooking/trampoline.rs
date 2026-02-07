62a8ce1fbbe0f464f20217ed08991e07f32bac3c
Soham Nandy <soham.nandy2006@gmail.com>
2026-02-08 01:18:43 +0530

    initialise trampoline struct (todo: used iced / hde or some other disassembler)

--- src/hooking/trampoline.rs
+++ src/hooking/trampoline.rs
@@ -27,15 +27,13 @@ struct JMP_ABS_QWORD {
 }
 
 #[repr(C)]
-/// Represents a 64 bit absolute offset indirect call
 struct CALL_ABS_QWORD {
-    opcode_first: u8, // FF15 00000002: CALL [+6]
-    opcode_second: u8,
-    dummy: u32,
-    dummy2: u8, // EB 08:         JMP +10
-    dummy3: u8, // 90:            NOP
-    /// CALL offset
-    offset: u64,
+    opcode0: u8, // FF
+    opcode1: u8, // 15
+    disp: u32,   // 2
+    jmp0: u8,    // EB
+    jmp1: u8,    // 08
+    address: u64,
 }
 
 #[repr(C)]
@@ -65,4 +63,53 @@ pub struct Trampoline {
     num_ip: u32,
     old_ip: Vec<u8>,
     new_ip: Vec<u8>,
+}
+
+impl Trampoline {
+    pub fn create_tramp() -> Result<Self, String> {
+        let mut ct = Trampoline {
+            target: std::ptr::null_mut(),
+            detour: std::ptr::null_mut(),
+            trampoline: std::ptr::null_mut(),
+            relay: std::ptr::null_mut(),
+            patch_above: false,
+            num_ip: 0,
+            old_ip: Vec::new(),
+            new_ip: Vec::new(),
+        };
+        let call = CALL_ABS_QWORD {
+            opcode0: 0xFF,
+            opcode1: 0x15,
+            disp: 2,
+            jmp0: 0xEB,
+            jmp1: 0x08,
+            address: 0x0000000000000000,
+        };
+
+        let jmp = JMP_ABS_QWORD {
+            opcode_first: 0xFF,
+            opcode_second: 0x25,
+            dummy: 0x00000000,
+            offset: 0x0000000000000000,
+        };
+
+        let jcc = JCC_ABS_QWORD {
+            opcode_first: 0x75,
+            dummy: 0x0E,
+            dummy2: 0xFF,
+            dummy3: 0x25,
+            dummy4: 0x00000000,
+            offset: 0x0000000000000000,
+        };
+
+        let mut old_pos = 0;
+        let mut new_pos = 0;
+        let mut jmp_dest: usize = 0; // dest of internal jump
+
+        let mut finished = false;
+        let mut instruction_buffer = [0u8; 16];
+
+        ct.patch_above = false;
+        todo!()
+    }
 }
