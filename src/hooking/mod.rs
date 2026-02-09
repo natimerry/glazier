1153e41a4ac058570dde052b3e9fc2ebf6c75b66
Soham Nandy <soham.nandy2006@gmail.com>
2026-02-09 19:57:58 +0530

    ditch trampoline for now

--- src/hooking/mod.rs
+++ src/hooking/mod.rs
@@ -1,8 +1,17 @@
-pub mod trampoline;
+pub mod pattern;
+
+use crate::ExpError;
 
 struct HookEntry {
-    address: usize,
-    original_bytes: Vec<u8>,
-    replacement_bytes: Vec<u8>,
+    target: *mut u8,
+    /// Target of the hook
+    detour: *mut u8,
+    /// Address of function to jmp to
+    trampoline: *mut u8,
+    /// Address of trampoline
+    hot_patch: bool,
     enabled: bool,
+    n_ip: u8, // Instruction boundary count
+    old_ips: Vec<u8>,
+    new_ips: Vec<u8>,
 }
